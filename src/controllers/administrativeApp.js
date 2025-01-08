08c21c795c07fb137b76157179fd5432ea5244be
JuanTobon7 <jctobon@unillanos.edu.co>
2025-01-07 22:29:17 -0500

    feat: Add conditional email sending logic in updateLead function

--- src/controllers/administrativeApp.js
+++ src/controllers/administrativeApp.js
@@ -27,6 +27,7 @@ exports.updateLead = async (req, res) => {
       res.status(400).send({ message: result.message })
       return
     }
+    // send email hacer condicional
     res.status(200).send(result)
   } catch (e) {
     res.status(500).send(`Ups algo fallo en el servidor ${e.message}`)
